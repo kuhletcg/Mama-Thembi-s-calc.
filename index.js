eba6cc64868fe2de40fe47b37bcf4f9a3cdfa672
Moral <jeramoral@gmail.com>
2020-07-14 14:50:39 +0000

    calculator

---
+++ index.js
@@ -0,0 +1,7 @@
+const numberButton = document.querySelectorAll('[data-number]')
+const operationButton = document.querySelectorAll('[data-operation]')
+const equalButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operation]')
+const currentOperandTextElement = document.querySelector('[data-current-operation]')
